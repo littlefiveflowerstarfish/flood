f258920c18d9e66ce706a5e33f153b0e85907e61
snow <littlefiveflowerstarfishai@gmail.com>
2025-02-18 13:34:30 -0500

    update image scripts

---
+++ infer_cls.sh
@@ -0,0 +1,9 @@
+python infer_cls.py  --tag a1 --data val --gpu 0 --fold 0 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data val --gpu 0 --fold 1 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data val --gpu 0 --fold 2 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data val --gpu 0 --fold 3 --data_dir /raid/ml/flood/b3_images
+
+python infer_cls.py  --tag a1 --data test --gpu 0 --fold 0 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data test --gpu 0 --fold 1 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data test --gpu 0 --fold 2 --data_dir /raid/ml/flood/b3_images
+python infer_cls.py  --tag a1 --data test --gpu 0 --fold 3 --data_dir /raid/ml/flood/b3_images
---
+++ train_cls.sh
@@ -0,0 +1,4 @@
+python train_cls.py --model yolo11m-cls --tag a1 --gpu 0 --fold 0
+python train_cls.py --model yolo11m-cls --tag a1 --gpu 0 --fold 1
+python train_cls.py --model yolo11m-cls --tag a1 --gpu 0 --fold 2
+python train_cls.py --model yolo11m-cls --tag a1 --gpu 0 --fold 3
